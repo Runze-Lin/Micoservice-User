edf162c3cac519417e949b15b11566f1c771d1c7
Isa Wang <74472229+IsaWang-05@users.noreply.github.com>
2023-11-16 17:19:33 -0500

    update database access

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 from fastapi import FastAPI, Response
-
-# I like to launch directly and not use the standard FastAPI startup
 import uvicorn
-
 from resources.students import StudentsResource
+import mysql.connector
+from mysql.connector import Error
 
 app = FastAPI()
 
-students_resource = StudentsResource()
+# Database connection
+def create_server_connection(host_name, user_name, user_password, db_name, port):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name,
+            port=port
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
 
+    return connection
+
+# Replace with your RDS credentials
+connection = create_server_connection("user-db.c6mxanhdzdn0.us-east-2.rds.amazonaws.com", "admin", "12345678", "users", 3306)
+
+students_resource = StudentsResource()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Awesome cloud developer 'Rebirth: I am Bezos' says hello {name}"}
-
 
 @app.get("/hello_text/{name}")
 async def say_hello_text(name: str):
@@ -26,12 +42,18 @@ async def say_hello_text(name: str):
     rsp = Response(content=the_message, media_type="text/plain")
     return rsp
 
-
-@app.get("/students")
-async def get_students():
-    result = students_resource.get_students()
+@app.get("/users")
+async def get_users():
+    result = []
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        result = cursor.fetchall()
+        cursor.close()
+    except Error as err:
+        print(f"Error: '{err}'")
+        result = {"error": str(err)}
     return result
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8012)
