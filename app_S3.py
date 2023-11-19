3cfab5be3e40989d8eda87abd5f923c470791e9d
Runze-Lin <rl3376@columbia.edu>
2023-11-19 18:37:12 -0500

    Update app_S3.py

--- app_S3.py
+++ app_S3.py
@@ -16,7 +16,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["https://6156staticweb.s3.amazonaws.com"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
